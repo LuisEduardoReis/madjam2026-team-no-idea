573bf795504bf76032fd4e5f3fa62ce837fc9ca4
Luis Reis <luiseduardo.mreis@gmail.com>
2026-02-28 19:42:27 +0000

    [Code] Add rocks

---
+++ src/world/entities/decoration/rock1.ts
@@ -0,0 +1,27 @@
+import {WorldEntity, type WorldEntityProps} from "@src/world/entities/world-entity";
+import type {Sprite} from "@src/graphics/sprite";
+import {getTexture} from "@src/graphics/textures";
+import p5 from "p5";
+import {drawSprite} from "@src/graphics/sprites-renderer";
+import type {Texture} from "@src/graphics/texture";
+
+
+export class Rock1 extends WorldEntity {
+
+    public texture: Texture;
+
+    constructor(props: WorldEntityProps) {
+        super(props);
+
+        this.collidesWithOthers = true;
+        this.collidesWithLevel = false;
+        this.immovable = true;
+        this.radius = 0.4;
+
+        this.texture = getTexture("sprites/rock/rock_000");
+    }
+
+    draw() {
+        drawSprite(this.world, this.texture, this.x,this.y, 0.5, 1,1);
+    }
+}
---
+++ src/world/entities/decoration/rock2.ts
@@ -0,0 +1,25 @@
+import {WorldEntity, type WorldEntityProps} from "@src/world/entities/world-entity";
+import {getTexture} from "@src/graphics/textures";
+import {drawSprite} from "@src/graphics/sprites-renderer";
+import type {Texture} from "@src/graphics/texture";
+
+
+export class Rock2 extends WorldEntity {
+
+    public texture: Texture;
+
+    constructor(props: WorldEntityProps) {
+        super(props);
+
+        this.collidesWithOthers = true;
+        this.collidesWithLevel = false;
+        this.immovable = true;
+        this.radius = 0.4;
+
+        this.texture = getTexture("sprites/rock/rock_001");
+    }
+
+    draw() {
+        drawSprite(this.world, this.texture, this.x,this.y, 0.5, 1,1);
+    }
+}
